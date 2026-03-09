6b00bfa574ac7613d60daabd6430a0918f81e80d
SuJeet Mehra <sujeet@apiit.edu.in>
2026-03-09 10:45:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const dbConnect = require("./database/db-connection");
+const router = require("./routes/auth.route");
+// middleware for json format accept data from client
+app.use(express.json());
 
-const dbConnect = require('./database/db-connection')
+http://localhost:4001/api
+app.use("/api", router);
 
 // first parametr - path, second - function, / root path
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-const port = 4001
+const port = 4001;
 app.listen(port, function () {
-    console.log(`Server listening on port http://localhost:${port}`);
-    dbConnect();
+  console.log(`Server listening on port http://localhost:${port}`);
+  dbConnect();
 });
